757be4781c6481e818f288f49f4737eb5abceba0
Simon Wallner <me@simonwallner.at>
2012-06-12 16:48:09 +0200

    added simple mouse tracking, and drawing a trailing line.

--- fitts-law.js
+++ fitts-law.js
@@ -3,6 +3,84 @@ var height = 400;
 var margin = {top: 30, right: 30, bottom: 30, left: 30};
 
 
+
+var fittsTest = {
+	target: {x: 0, y: 0, r: 10},
+	start: {x: 0, y: 0, time: 0},
+	last: {x: 0, y: 0},
+	currentPath: [],
+	active: false,
+	
+	generateTarget: function() {
+		this.target.x = randomAB(margin.left, width);
+		this.target.y = randomAB(margin.top, height);
+		svg.append('svg:circle')
+			.attr('id', 'target')
+			.attr('cx', this.target.x)
+			.attr('cy', this.target.y)
+			.attr('r', this.target.r)
+			.style('fill', 'red');
+		
+		this.active = true;
+	},
+	
+	removeTarget: function() {
+		svg.selectAll('#target').remove();
+		this.active = false;
+	},
+	
+	hitTarget: function(x, y) {
+		var dx = this.target.x - x;
+		var dy = this.target.y - y;
+
+		return (Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)) < this.target.r);
+	},
+	
+	mouseMoved: function(x, y) {
+		if (this.active) {
+			var newPoint = {x: x, y: y}
+			this.currentPath.push(newPoint)
+			
+			svg.append('svg:line')
+				.attr('class', 'path')
+				.attr('x1', this.last.x)
+				.attr('x2', newPoint.x)
+				.attr('y1', this.last.y)
+				.attr('y2', newPoint.y)
+				.transition()
+					.duration(2000)
+					.style('stroke-opacity', 0)
+					.remove();
+				
+			this.last = newPoint;
+		}
+	}
+}
+
+function randomAB(a, b) {
+	return a + Math.random() * b;
+}
+
+function mouseMoved()
+{
+	var m = d3.svg.mouse(this);
+	fittsTest.mouseMoved(m[0], m[1])
+}
+
+function mouseClicked()
+{
+	var mouse = d3.svg.mouse(this);
+	if (fittsTest.hitTarget(mouse[0], mouse[1])) {
+		fittsTest.removeTarget();
+		fittsTest.last = {x: mouse[0], y: mouse[1]};
+		fittsTest.generateTarget();
+	}
+}
+
+
+
+
+
 svg = d3.select('#test-area').append('svg')
 	.attr('width', width + margin.left + margin.right)
 	.attr('height', height + margin.top + margin.bottom)
@@ -16,54 +94,6 @@ svg.append('rect')
 	.attr('width', width + margin.left + margin.right)
 	.attr('height', height + margin.top + margin.bottom)
 	.attr('class', 'back');
-	
 
-var fittsTest = {
-	pos: {x: 0, y: 0},
-	active: false,
-	
-	generateTarget: function() {
-		this.pos.x = randomAB(margin.left, width);
-		this.pos.y = randomAB(margin.top, height);
-		svg.append('svg:circle')
-			.attr('id', '#target')
-			.attr('cx', this.pos.x)
-			.attr('cy', this.pos.y)
-			.attr('r', 10)
-			.style('fill', 'red');
-		
-		this.active = true;
-	},
-	
-	removeTarget: function() {
-		svg.selectAll('#target').remove();
-		this.active = false;
-	}
-}
 
-function randomAB(a, b) {
-	return a + Math.random() * b;
-}
-
-function mouseMoved()
-{
-	var m = d3.svg.mouse(this);
-	
-	svg.append('svg:circle')
-	      .attr('cx', m[0])
-	      .attr('cy', m[1])
-	      .attr('r', 10)
-	      .style('stroke', 'gold')
-		  .style('fill', 'gold')
-	      .style('stroke-opacity', 1)
-		  .transition()
-			      .duration(2000)
-			      .attr('r', 0)
-			      .style('stroke-opacity', 1e-6)
-		      	.remove();
-}
-
-function mouseClicked()
-{
-	fittsTest.generateTarget();
-}
+fittsTest.generateTarget();
